71780567c3f9d24f29ff5b4707fdebbbceb30954
drjpoudel <drjpoudel@gmail.com>
2025-08-28 11:30:49 +0545

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
-
+    <footer class="site-footer">
+        <p>
+            <?php
+                $options = get_option('stotramala_theme_settings');
+                echo esc_html($options['footer_text'] ?? '&copy; ' . date('Y') . ' ' . get_bloginfo('name'));
+            ?>
+        </p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
